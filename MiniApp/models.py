91ce180de185abd9380ed8fb8963506e52d0f4d7
unknown <elmehdi.lghaouch-etu@etu.univh2c.ma>
2022-11-14 11:48:01 +0100

    first commit

---
+++ MiniApp/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+# Create your models here.
+class UserProfileInfo(models.Model):
+
+    # Create relationship (don't inherit from User!)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # Add any additional attributes you want
+    portfolio_site = models.URLField(blank=True)
+    # pip install pillow to use this!
+    # Optional: pip install pillow --global-option=”build_ext” --global-option=”--disable-jpeg”
+    profile_pic = models.ImageField(upload_to='profile_pics',blank=True)
+
+    def __str__(self):
+        # Built-in attribute of django.contrib.auth.models.User !
+        return self.user.username
+
+class Employee(models.Model):
+    eid = models.CharField(max_length=20)
+    ename = models.CharField(max_length=100)
+    eemail = models.EmailField()
+    econtact = models.CharField(max_length=15)
+    class Meta:
+        db_table = "employee"
